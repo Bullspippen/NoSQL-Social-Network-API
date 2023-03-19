29817e8d9afa92e4838d09566c720cd2cd788623
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-18 22:52:44 -0400

    updated comments on thoughtRoutes.js

--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -17,22 +17,19 @@ const Thought = require("../../models/Thought");
 // Set up GET all and POST at /api/thoughts
 router.route("/").get(getAllThoughts).post(createThought);
 
-// Set up GET one, PUT, and DELETE at /api/thoughts/:id
-router
-  .route("/:thoughtId").get(getThoughtById);
+// Set up GET one at /api/thoughts/:id
+router.route("/:thoughtId").get(getThoughtById);
 
-  // Set up GET one, PUT, and DELETE at /api/thoughts/:id
-router
-.route("/:thoughtId").put(updateThought);
+// Set up PUT at /api/thoughts/:id
+router.route("/thoughts/:thoughtId").put(updateThought);
 
-// Set up GET one, PUT, and DELETE at /api/thoughts/:id
-router
-  .route("/:thoughtId/").delete(deleteThought);
+// Set up DELETE at /api/thoughts/:id
+router.route("/thoughts/:thoughtId/").delete(deleteThought);
 
-// Set up POST and DELETE at /api/thoughts/:thoughtId/reactions
+// Set up POST at /api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(addReaction);
 
-// Set up POST and DELETE at /api/thoughts/:thoughtId/reactions
+// Set up DELETE at /api/thoughts/:thoughtId/reactions/:reactionId
 router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
