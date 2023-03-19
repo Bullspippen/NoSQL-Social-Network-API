365ffb176606253665885bc7528d00c28dadd4b2
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-18 23:09:02 -0400

    circled back to original route format on thoughtRoutes.js file prior to all of the changes

--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -17,19 +17,17 @@ const Thought = require("../../models/Thought");
 // Set up GET all and POST at /api/thoughts
 router.route("/").get(getAllThoughts).post(createThought);
 
-// Set up GET one at /api/thoughts/:id
-router.route("/:thoughtId").get(getThoughtById);
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
+router
+  .route("/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
-// Set up PUT at /api/thoughts/:id
-router.route("/thoughts/:thoughtId").put(updateThought);
-
-// Set up DELETE at /api/thoughts/:id
-router.route("/thoughts/:thoughtId/").delete(deleteThought);
-
-// Set up POST at /api/thoughts/:thoughtId/reactions
+// Set up POST and DELETE at /api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(addReaction);
 
-// Set up DELETE at /api/thoughts/:thoughtId/reactions/:reactionId
+// Set up POST and DELETE at /api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
